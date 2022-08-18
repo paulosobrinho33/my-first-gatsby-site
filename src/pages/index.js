c5c9f02edc5983b77d9182fe708cc5dc70e9c6dd
Paulo Sobrinho <paulo.sobrinho33@gmail.com>
2022-08-18 19:57:38 -0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Spirali cover image"
+        src="../images/cover-spirali.jpg"
+      />
     </Layout>
   );
 };
